cb427c6a9a4baf8488dc467ffbc1b276d06e9dc3
Fluqz <robin@hlawinka.de>
2023-11-26 18:59:11 +0100

    Adding animations - Angular animations tend to never work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { SidemenuComponent } from './views/sidemenu/sidemenu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [NotificationService],
   bootstrap: [AppComponent]
--- src/app/views/templates/notification/notification.component.ts
+++ src/app/views/templates/notification/notification.component.ts
@@ -1,21 +1,53 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding, Input } from '@angular/core';
 import { INotification } from 'src/app/provider/notification.service';
 
 @Component({
   selector: 'notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss']
+  styleUrls: ['./notification.component.scss'],
+  animations: [
+    // trigger('slideInOut', [
+    //   transition(':enter', [
+    //     style({transform: 'translateX(-100%)'}),
+    //     animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+    //   ]),
+    //   transition(':leave', [
+    //     animate('200ms ease-in', style({transform: 'translateX(-100%)'}))
+    //   ])
+    // ])
+    trigger("slideInOut", [
+      transition(":enter", [
+          style({ opacity: 0, transform: "translateX(-100%)" }), //apply default styles before animation starts
+          animate(
+              "2s ease-in-out",
+              style({ opacity: 1, transform: "translateX(0)" })
+          )
+      ]),
+      transition(":leave", [
+          style({ opacity: 1, transform: "translateX(0)" }), //apply default styles before animation starts
+          animate(
+              "2s ease-in-out",
+              style({ opacity: 0, transform: "translateX(-100%)" })
+          )
+      ])
+  ])
+  ]
 })
 export class NotificationComponent {
 
   private _botPos: number = 10
 
+  /** INotification data. */
   @Input('notification') notification: INotification
+  /** Order number. Index in the NotificationService.notifications array. */
   @Input('index') set index(i: number) {
 
+    // Position notifications below each other. Newest top oldest bot.
     this.bottom = i == 0 ? this._botPos : (i * 45) + this._botPos
   }
 
+  /** Bind css bottom attribute to host. */
   @HostBinding('style.bottom.px') bottom: number = this._botPos
 
 }
